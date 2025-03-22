e87f590021ca294c426abb02e7bc54449be99627
VladaZh <vladislava@MacBook-Pro-Vladislava.local>
2025-03-22 09:33:51 +0300

    Базовый функционал

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.notes
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -13,39 +14,25 @@ import com.example.notes.db.MyDbManager
 
 class MainActivity : AppCompatActivity() {
     val myDbManager = MyDbManager(context = this)
-    lateinit var edTitle: EditText
-    lateinit var edContent: EditText
-    lateinit var tvTest: TextView
+
 //    val edTitle = findViewById<EditText>(edTitle)
 //    val edContent = findViewById(R.id.edContent)
 //    val tvTest = findViewById(R.id.tvTest)
     override fun onResume() {
         super.onResume()
         myDbManager.openDb()
-        val dataList = myDbManager.readDbData()
-        for (item in dataList){
-            tvTest.append(item)
-            tvTest.append("\n")
-        }
     }
+
+    fun onClickNew(view: View){
+        val i = Intent(this, EditActivity::class.java)
+        startActivity(i)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        edTitle = findViewById(R.id.edTitle)
-        edContent = findViewById(R.id.edContent)
-        tvTest = findViewById(R.id.tvTest)
 
-    }
-
-    fun onClickSave(view: View) {
-        tvTest.text = ""
-        myDbManager.insertToDb(edTitle.text.toString(), edContent.text.toString())
-        val dataList = myDbManager.readDbData()
-        for (item in dataList){
-            tvTest.append(item)
-            tvTest.append("\n")
-        }
     }
 
     override fun onDestroy() {
